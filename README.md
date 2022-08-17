1df1ff63dbb244c23d3ed2f9aa49f6056479b755
The Anh Pham <admin@phamtheanh.eu>
2022-08-17 21:22:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vite-vue3-stylus-template
+ Vite + Vue3 + Stylus template
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div id="my-div">THIS IS MY DIV</div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped lang="stylus">
+@import "./assets/global.styl";
+
+#my-div {
+  padding: 1rem;
+  color: $c-text;
+  background-color: $c-bg;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
